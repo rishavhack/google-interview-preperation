3395f7a37cdc97450322be87679ee4fc500a94b7
ACCOLITE\rishav <rishav@accolitedigital.com>
2024-12-07 09:16:57 +0530

    Added count unique value

--- common-question/countUniqueValue.js
+++ common-question/countUniqueValue.js
@@ -12,3 +12,21 @@ console.log(countUniqueValue([1, 1, 1, 1, 1, 2]));
 console.log(countUniqueValue([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
 console.log(countUniqueValue([]));
 console.log(countUniqueValue([-2, -1, -1, 0, 1]));
+
+function countUniqueValue(arr) {
+  if (!arr.length) {
+    return 0;
+  }
+  var i = 0;
+  for (let j = i + 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+console.log(countUniqueValue([1, 1, 1, 1, 1, 2])); //2
+console.log(countUniqueValue([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])); //7
+console.log(countUniqueValue([]));
+console.log(countUniqueValue([-2, -1, -1, 0, 1])); // 4
